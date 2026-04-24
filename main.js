387c2a6c5adfd7986576e30c6d18361c82fb1b97
BasmalaMoslam6 <basbosa.00.m@gmail.com>
2026-04-24 18:51:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,340 @@
+let employees = [
+    {
+        name: "employee1",
+        age: 30,
+        jobTitle: "Software Engineer",
+        isManager : true,
+        address: {city: "Cairo", country:"Egypt"},
+        skills:["HTML", "CSS", "JS"],
+        salary: [5000, 5500 , 6000]
+    },
+    {
+        name: "employee2",
+        age: 25,
+        jobTitle: "UI Designer",
+        isManager : false,
+        address: {city: "Alex", country:"Egypt"},
+        skills:["Figma", "Photoshop"],
+        salary: [4000, 4200 , 4500]
+    },
+    {
+        name: "employee3",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee4",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee5",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee6",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee7",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee8",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee9",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee10",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee11",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee12",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee13",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee14",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee15",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee16",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee17",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee18",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee19",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee20",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee21",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee22",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee23",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee24",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee25",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee26",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee27",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee28",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee29",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    },
+    {
+        name: "employee30",
+        age: 28,
+        jobTitle: "Backend Dev",
+        isManager : false,
+        address: {city: "Giza", country:"Egypt"},
+        skills:["Node", "MongoDB"],
+        salary: [4500, 4700 , 5000]
+    }
+];
+function avg(arr) {
+    return arr.reduce((a,b)=>a+b,0)/arr.length;
+}
+function totalSalary(){
+    let total = 0;
+    employees.forEach(emp => {
+        emp.salary.forEach(s => total += s);
+    });
+    document.getElementById("totalSalary").innerText=
+    "Total company Salary: " + total;
+}
+
+function render(list){
+    let container = document.getElementById("employeesContainer");
+    container.innerHTML ="";
+
+    list.forEach(emp => {
+        let card = document.createElement("div");
+        card.classList = "card";
+
+        card.innerHTML = 
+        `<h2>${emp.name}</h2>
+        <p>${emp.jobTitle}</p>
+        <button onclick="toggleDetailes(this)">Show Details</button>
+        
+        <div class="details">
+            <p>Age : ${emp.age}</p>
+            <p>Manager : ${emp.isManager ? "Yes" : "No"}</p>
+            <p>City : ${emp.address.city}</p>
+            <p>Skills : ${emp.skills.join(", ")}</p>
+            <p>Salary : ${emp.salary.join(", ")}</p>
+            <p>Avg Salary : ${avg(emp.salary).toFixed(2)}</p>
+        </div>`
+        ;
+        container.appendChild(card);
+    });
+}
+function toggleDetailes(btn){
+    let card = btn.parentElement;
+    card.classList.toggle("active");
+
+    if(card.classList.contains("active")){
+        btn.innerText = "Hide Details";
+    }else{
+        btn.innerText = "Show Details";
+    }
+}
+document.getElementById("search").addEventListener("input", function(){
+    let value = this.value.toLowerCase();
+    let filtered = employees.filter(e => 
+        e.name.toLowerCase().includes(value)
+    );
+    render(filtered);
+});
+document.getElementById("filter").addEventListener("change", function(){
+    if(this.value === "manager") {
+        render(employees.filter(e => e.isManager));
+    }else{
+        render(employees)
+    }
+});
+render(employees);
+totalSalary();
+
+
+
+
+
